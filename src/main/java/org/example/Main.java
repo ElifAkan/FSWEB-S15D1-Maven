03f97136e23e46096d1acd059336b6e5f46e991a
ElifAkan <elif.akan.2002@hotmail.com>
2024-10-04 19:33:39 +0300

    S15D1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,17 @@
 package org.example;
 
+import org.example.mobile.Contact;
+import org.example.mobile.MobilePhone;
+import org.example.models.Grocery;
+
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-
+      //  Grocery grocery= new Grocery();
+      //  grocery.startGrocery();
+        MobilePhone mobilePhone=new MobilePhone("233456765", new ArrayList<>());
+        mobilePhone.addNewContact(new Contact("elif","11213"));
+        mobilePhone.addNewContact(new Contact("ela","16213"));
     }
 }
